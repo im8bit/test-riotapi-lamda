56691005c27ff4bc22f8fffd035d3e2bd350e022
Juan Mora <jmora@internetbrands.com>
2022-03-09 19:45:53 -0800

    Move functions to a module

--- go.mod
+++ go.mod
@@ -3,7 +3,11 @@ module test-riotapi-lambda/main
 go 1.17
 
 require (
-	github.com/aws/aws-lambda-go v1.28.0 // indirect
-	github.com/aws/aws-sdk-go v1.42.52 // indirect
-	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/aws/aws-lambda-go v1.28.0
+	github.com/aws/aws-sdk-go v1.43.15
+	github.com/im8bit/test-riotapi-library v0.0.0-00010101000000-000000000000
 )
+
+require github.com/jmespath/go-jmespath v0.4.0 // indirect
+
+replace github.com/im8bit/test-riotapi-library => ../test-riotapi-library
--- main.go
+++ main.go
@@ -2,80 +2,29 @@ package main
 
 import (
 	"context"
-	"log"
 
 	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
-	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
-	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbiface"
-	"github.com/aws/aws-sdk-go/service/dynamodb/expression"
+
+	"github.com/im8bit/test-riotapi-library/aws"
+	"github.com/im8bit/test-riotapi-library/riot"
 )
 
 type MyEvent struct {
 	Name string `json:"name"`
 }
 
-/*
-	func HandleRequest(ctx context.Context, name MyEvent) (string, error) {
-		return fmt.Sprintf("Hello %s!", name.Name), nil
-	}
-*/
-
-type leaderboardDynamoDBItem struct {
-	ActId    string `json:"actid"`
-	Puuid    string `json:"puuid"`
-	GameName string `json:"gameName"`
-	TagLine  string `json:"tagLine"`
-	Rank     int    `json:"leaderboardRank"`
-	Rating   int    `json:"rankedRating"`
-	Wins     int    `json:"numberOfWins"`
-}
-
-var activeActId string = "573f53ac-41a5-3a7d-d9ce-d6a6298e5704"
-var tablename = "val_leaderboards"
-
-func HandleRequest(ctx context.Context, name MyEvent) ([]leaderboardDynamoDBItem, error) {
+func HandleRequest(ctx context.Context, name MyEvent) ([]aws.LeaderboardDynamoDBItem, error) {
 	sess := session.Must(session.NewSessionWithOptions(session.Options{
 		SharedConfigState: session.SharedConfigEnable,
 	}))
 	svc := dynamodb.New(sess)
-	result := findAll(svc, activeActId)
+
+	var activeActId string = riot.GetActiveActId()
+
+	result := aws.FindAll(svc, activeActId)
 	return result, nil
-}
-
-func findAll(svc dynamodbiface.DynamoDBAPI, actid string) []leaderboardDynamoDBItem {
-	filt := expression.Name("actid").Equal(expression.Value(actid))
-	proj := expression.NamesList(expression.Name("gameName"), expression.Name("leaderboardRank"), expression.Name("numberOfWins"))
-
-	expr, err := expression.NewBuilder().WithFilter(filt).WithProjection(proj).Build()
-	if err != nil {
-		log.Fatalf("Got error building expression: %s", err)
-	}
-
-	// Build the query input parameters
-	params := &dynamodb.ScanInput{
-		ExpressionAttributeNames:  expr.Names(),
-		ExpressionAttributeValues: expr.Values(),
-		FilterExpression:          expr.Filter(),
-		ProjectionExpression:      expr.Projection(),
-		TableName:                 aws.String(tablename),
-	}
-
-	// Make the DynamoDB Query API call
-	result, err := svc.Scan(params)
-	if err != nil {
-		log.Fatalf("Query API call failed: %s", err)
-	}
-
-	itemList := []leaderboardDynamoDBItem{}
-	err = dynamodbattribute.UnmarshalListOfMaps(result.Items, &itemList)
-	if err != nil {
-		log.Fatalf("Got error unmarshalling: %s", err)
-	}
-
-	return itemList
 }
 
 func main() {
